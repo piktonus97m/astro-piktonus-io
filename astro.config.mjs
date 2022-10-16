3d33083369515189b2d3390a730fa3c6535e4b51
102563271+lancerossdev@users.noreply.github.com <102563271+lancerossdev@users.noreply.github.com>
2022-10-16 14:19:03 +0800

    new update yay

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lanceross.xyz',
+  site: 'https://basicblog.lanceross.xyz',
   base: '/',
   vite: {
     ssr: {
